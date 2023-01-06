19acf65bec5c4cdf93424e990c4d03ff472fefb1
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-01-06 17:24:33 -0300

    payroll api created

---
+++ payroll-api/src/main/java/com/gvfs/payrollapi/feignClients/UserFeign.java
@@ -0,0 +1,23 @@
+package com.gvfs.payrollapi.feignClients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.gvfs.payrollapi.domain.User;
+
+
+@FeignClient(name = "user-api", url = "http://localhost:8000")
+public interface UserFeign {
+	
+	@GetMapping(value = "/api/users/{id}")
+	ResponseEntity<User> findById(@PathVariable Long id);
+	
+	@GetMapping
+	ResponseEntity<List<User>> findAll();
+
+
+}
