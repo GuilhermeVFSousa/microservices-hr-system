e08be1a70a7a22cbe88841379a947d11088f8ff7
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-01-07 11:51:35 -0300

    naming server with netflix eureka implemented

--- user-api/src/main/java/com/gvfs/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/com/gvfs/userapi/services/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.gvfs.userapi.services.impl;
 
 import java.util.List;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.gvfs.userapi.domain.User;
@@ -10,16 +11,20 @@ import com.gvfs.userapi.services.UserService;
 import com.gvfs.userapi.services.exceptions.ObjectNotFoundException;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j //log.info
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 	
 	
 	private final UserRepository repository; //substitui a anotação @autowired (precisa haver um construtor)
+	private final Environment env;
 
 	@Override
 	public User findById(Long id) {
+		log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
 		return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found."));
 	}
 
